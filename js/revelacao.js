3979c495cfcdd7ba328267c57203840aad12feda
idail <neto_br_8@hotmail.com>
2024-01-17 11:15:38 -0400

    Sendo ajustado o site

--- js/revelacao.js
+++ js/revelacao.js
@@ -1,7 +1,20 @@
+debugger;
 window.relevar = ScrollReveal({reset:true});
 
 relevar.reveal(".section-trabalho",
 {
     duration:2000,
     distance:"90px"
-})
+});
+
+relevar.reveal("#informacoes_seguindo_cliente",
+{
+    duration:2000,
+    distance:"90px"
+});
+
+relevar.reveal("#projetos",
+{
+    duration:2000,
+    distance:"90px"
+});

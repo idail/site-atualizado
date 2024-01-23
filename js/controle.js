e1d8ed41a0a5a8c52cd2c6d315fe0e3ffe041b0a
idail <neto_br_8@hotmail.com>
2024-01-23 09:50:44 -0400

    Ajuste site

---
+++ js/controle.js
@@ -0,0 +1,9 @@
+$(document).ready(function(e){
+    debugger;
+    var url = window.location.href;
+
+    if(url === "https://www.idailneto.com.br/site/")
+    {
+        window.location.href = "https://www.idailneto.com.br";
+    }
+});
